ae8fcaa684ea475b4ee8bcd61576a209ea769216
Daniel Jiménez <danielji@student.42madrid.com>
2026-01-19 17:42:28 +0100

    multi line macros

--- CPP01/ex05/Harl.hpp
+++ CPP01/ex05/Harl.hpp
@@ -7,12 +7,9 @@
 # define INFO "INFO"
 # define WARNING "WARNING"
 # define ERROR "ERROR"
-# define DEBUG_MSG "I love having extra bacon for my 7XL-double-cheese-triple-\
-	pickle-special-ketchup burger. I really do!"
-# define INFO_MSG "I cannot believe adding extra bacon costs more money. You d\
-	idn't put enough bacon in my burger! If you did, I wouldn't be asking for more!"
-# define WARNING_MSG "I think I deserve to have some extra bacon for free. I'v\
-	e been coming for years, whereas you started working here just last month."
+# define DEBUG_MSG "I love having extra bacon for my 7XL-double-cheese-triple-pickle-special-ketchup burger. I really do!"
+# define INFO_MSG "I cannot believe adding extra bacon costs more money. You didn't put enough bacon in my burger! If you did, I wouldn't be asking for more!"
+# define WARNING_MSG "I think I deserve to have some extra bacon for free. I've been coming for years, whereas you started working here just last month."
 # define ERROR_MSG "This is unacceptable! I want to speak to the manager now."
 
 class Harl
