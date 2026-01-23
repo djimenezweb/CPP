88314e138dc06f6d2cd514ea9210dfeb1a8243de
djimenezweb <djimenezweb@gmail.com>
2026-01-23 09:45:07 +0100

    ex00 & ex01

--- CPP02/ex01/Fixed.hpp
+++ CPP02/ex01/Fixed.hpp
@@ -4,6 +4,8 @@
 #include <iostream>
 
 #define DEF_CONSTR "Default constructor called"
+#define INT_CONSTR "Int constructor called"
+#define FLT_CONSTR "Float constructor called"
 #define DESTRUCTOR "Destructor called"
 #define CPY_CONSTR "Copy constructor called"
 #define CPY_ASSIGN "Copy assignment operator called"
@@ -16,13 +18,19 @@ private:
 	static const int fr_bits;
 
 public:
-	Fixed();				   // default constructor
+	Fixed(); // default constructor
+	Fixed(const int value);
 	Fixed(const Fixed &other); // copy constructor
 	//void operator=(const Fixed &other); // copy assignment operator overload
-	Fixed &operator=(const Fixed &other); // copy assignment operator overload
-	~Fixed();							  // destructor
+	Fixed &operator=(const Fixed &other);  // copy assignment operator overload
+	Fixed &operator<<(const Fixed &other); // copy insertion operator overload
+	~Fixed();							   // destructor
+
 	int getRawBits(void) const;
 	void setRawBits(int const raw);
+
+	float toFloat(void) const;
+	int toInt(void) const;
 };
 
 /*
