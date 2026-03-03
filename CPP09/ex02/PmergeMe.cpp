e70afed81ca5f780e3d345424ea82408fc2c4840
Daniel Jiménez <danielji@student.42madrid.com>
2026-03-03 12:38:09 +0100

    Sort pairs

--- CPP09/ex02/PmergeMe.cpp
+++ CPP09/ex02/PmergeMe.cpp
@@ -4,7 +4,7 @@
 PmergeMe::PmergeMe() {}
 
 // Parameterized constructor
-PmergeMe::PmergeMe(char **argv) : argv(argv)
+PmergeMe::PmergeMe(int argc, char **argv) : argc(argc), argv(argv)
 {}
 
 // Copy constructor
@@ -26,3 +26,73 @@ PmergeMe &PmergeMe::operator=(const PmergeMe &other)
 
 // Destructor
 PmergeMe::~PmergeMe() {}
+
+void PmergeMe::initVector()
+{
+	int					number;
+	std::string			str;
+
+	for (int i = 1; i < argc; i++)
+	{
+		str = argv[i];
+		std::stringstream ss(str);
+		while (!ss.eof())
+		{
+			ss >> number;
+			if (ss.fail())
+				return;
+			v.push_back(number);
+		}
+	}
+}
+
+void PmergeMe::printVector(std::vector<int> &vector)
+{
+	std::cout << "{ ";
+	for (size_t i = 0; i < vector.size(); i++)
+	{
+		std::cout << vector[i] << " ";
+	}
+	std::cout << "}" << std::endl;
+}
+
+void PmergeMe::sort_by_pairs(std::vector<int> &vector)
+{
+	std::vector<int>::iterator it = vector.begin();
+	std::vector<int>::iterator it_end = vector.end();
+	while (it + 1 < it_end)
+	{
+		if (*it > *(it + 1))
+			std::iter_swap(it, it + 1);
+		it += 2;
+	}
+}
+
+/* void PmergeMe::divide_by_pairs_rec(std::vector<int> parent)
+{
+	if (parent.size() <= 2)
+		return;
+	std::vector<int>::iterator it = parent.begin();
+	std::vector<int>::iterator it_end = parent.end();
+	std::vector<int>::iterator it_mid = parent.begin() + parent.size() / 2;
+
+	std::vector<int> left(it, it_mid);
+	std::vector<int> right(it_mid, it_end);
+
+	std::cout << "{ ";
+	printVector(left);
+	std::cout << " }, { ";
+	printVector(right);
+	std::cout << " }" << std::endl;
+
+	divide_by_pairs_rec(left);
+	divide_by_pairs_rec(right);
+} */
+
+// Sort
+void PmergeMe::sort()
+{
+	printVector(v);
+	sort_by_pairs(v);
+	printVector(v);
+}
--- CPP09/ex02/PmergeMe.hpp
+++ CPP09/ex02/PmergeMe.hpp
@@ -2,18 +2,30 @@
 # define PMERGEME_H
 
 # include <iostream>
+# include <sstream>
+# include <vector>
+# include <algorithm>
 
 class PmergeMe
 {
 	private:
+		int argc;
 		char **argv;
+		std::vector<int> v;
+		PmergeMe();
 
 	public:
-		PmergeMe();
-		PmergeMe(char **argv);
+		PmergeMe(int argc, char **argv);
 		~PmergeMe();
 		PmergeMe(const PmergeMe &other);
 		PmergeMe &operator=(const PmergeMe &other);
+
+		void initVector();
+		void printVector(std::vector<int> &vector);
+		void sort();
+		void sort_by_pairs(std::vector<int> &vector);
+		// void divide_by_pairs();
+		// void divide_by_pairs_rec(std::vector<int> parent);
 };
 
 #endif
